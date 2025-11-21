a17a0af6822474b9b602d8e856ed5785eb275c6e
Nick Mangubat <nmang004@gmail.com>
2025-11-20 20:00:29 -0500

    Updated Resume Link

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -3,9 +3,9 @@ import './ResumeButton.css';
 
 const ResumeButton = () => {
     return (
-        <a 
-            href="https://drive.google.com/file/d/1MApRLOkxlTdECCpF8hcf8b9To86BG_hB/view" 
-            target="_blank" 
+        <a
+            href="https://drive.google.com/file/d/1Ux_Mze0mhd3xK0ZzH-XiEijVfMuM4z7q/view"
+            target="_blank"
             rel="noopener noreferrer"
             className="resume-button"
         >
